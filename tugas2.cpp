c9c569a7b7c87a3c596ccae8f1983c5521a582c8
VrixxioN <65044968+VrixxioN@users.noreply.github.com>
2021-03-07 13:55:21 +0800

    Update tugas2.cpp

--- tugas2.cpp
+++ tugas2.cpp
@@ -1,3 +1,5 @@
+// tugas 2 : program beberapa jenis perhitungan
+
 #include<iostream>
 #include<cmath>
 using namespace std;
@@ -9,52 +11,86 @@ void menu() {
 }
 
 int main() {
-    int choice;
-    double a, b;
+    int choice, x;
+    double a, b, atas, bawah, div, i, koef, sum, sumint;
     menu();
     cin >> choice;
     cout << endl;
     switch (choice) {
         case 1:
-        cout << "#Penjumlahan (a+b)#\n";
+        cout << "===Penjumlahan (a+b)===\n";
         cout << "Masukkan nilai a: ";
         cin >> a;
         cout << "Masukkan nilai b: ";
         cin >> b;
         cout << "a + b = " << a+b;
         break;
+
         case 2:
-        cout << "#Pengurangan (a-b)#\n";
+        cout << "===Pengurangan (a-b)===\n";
         cout << "Masukkan nilai a: ";
         cin >> a;
         cout << "Masukkan nilai b: ";
         cin >> b;
         cout << "a - b = " << a-b;
         break;
+
         case 3:
-        cout << "#Pembagian (a*b)#\n";
+        cout << "===Pembagian (a*b)===\n";
         cout << "Masukkan nilai a: ";
         cin >> a;
         cout << "Masukkan nilai b: ";
         cin >> b;
         cout << "a * b = " << a*b;
         break;
+
         case 4:
-        cout << "#Pembagian (a/b)#\n";
+        cout << "===Pembagian (a/b)===\n";
         cout << "Masukkan nilai a: ";
         cin >> a;
         cout << "Masukkan nilai b: ";
         cin >> b;
         cout << "a / b = " << a/b;
         break;
+
         case 5:
-        cout << "#Perpangkatan (a^(b))#\n";
+        cout << "===Perpangkatan (a^(b))===\n";
         cout << "Masukkan nilai a: ";
         cin >> a;
         cout << "Masukkan nilai b: ";
         cin >> b;
         cout << "a^(b) = " << pow(a,b);
-        break;       
+        break;
+
+        case 6:
+        cout << "===Integral Tentu dari fungsi (ax^n + bx^(n-1) + ... + konstanta)===\n";
+        cout << "berapa suku tertinggi dari fungsi yang ingin di integralkan : ";
+        cin >> x;
+        double integral[x+1];
+        for (int i = 0; i < x+1; i++) {
+            cout << "masukkan koefisien suku dengan pangkat " << i << " : ";
+            cin >> koef;
+            integral[i] = (koef);
+        }
+        cout << "Masukkan batas bawah dari integral : ";
+        cin >> bawah;
+        cout << "Masukkan batas atas dari integral : ";
+        cin >> atas;
+        div = 0.01;
+        sum = 0;
+        a = bawah;
+        b = bawah + div;
+        while (b <= atas) {
+            sumint = 0;
+            for (int i = 0; i < x+1; i++) {
+                sumint = sumint + integral[i]*(pow(a,i)+pow(b,i));
+            }
+            sum = sum + sumint*div*(0.5);
+            a = a + div;
+            b = b + div;   
+        }
+        cout << sum;
+        break;
     }
     return 0;
 }
